24cd57a308d6be7acbf8f136d5c4efae5c4174db
Yanwen Lin <lyw1124278064@gmail.com>
2026-05-18 11:32:26 -0700

    Add SDPA function;

--- cs336_basics/functions.py
+++ cs336_basics/functions.py
@@ -1,7 +1,24 @@
-from jaxtyping import Float
+import math
+from einops import einsum
+from jaxtyping import Float, Bool
 import torch
+from torch import Tensor
 
 
-def softmax(x: Float[torch.Tensor, "..."], dim: int) -> Float[torch.Tensor, " ..."]:
+def softmax(x: Float[Tensor, "..."], dim: int) -> Float[Tensor, " ..."]:
     exp = torch.exp(x - torch.max(x, dim=dim, keepdim=True)[0])
     return exp / torch.sum(exp, dim=dim, keepdim=True)
+
+
+def scaled_dot_product_attention(
+    Q: Float[Tensor, "batch_size ... seq_len d_k"],
+    K: Float[Tensor, "batch_size ... seq_len d_k"],
+    V: Float[Tensor, "batch_size ... seq_len d_v"],
+    mask: Bool[Tensor, "seq_len seq_len"] | None=None
+) -> Float[Tensor, "batch_size ... seq_len d_v"]:
+    d_k = Q.shape[-1]
+    pre_softmax = einsum(Q, K, "... n d_k, ... m d_k -> ... n m") / math.sqrt(d_k)
+    if mask is not None:
+        pre_softmax = pre_softmax + torch.zeros(mask.shape).masked_fill(~mask, float('-inf'))
+    post_softmax = softmax(pre_softmax, -1)
+    return einsum(post_softmax, V, "... n m, ... m d_v -> ... n d_v")
--- tests/adapters.py
+++ tests/adapters.py
@@ -123,7 +123,7 @@ def run_scaled_dot_product_attention(
     Returns:
         Float[Tensor, " ... queries d_v"]: Output of SDPA
     """
-    raise NotImplementedError
+    return functions.scaled_dot_product_attention(Q, K, V, mask)
 
 
 def run_multihead_self_attention(
