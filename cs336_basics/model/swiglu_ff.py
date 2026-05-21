450dd21ccbd6846d6725d69272843b054cf3b442
Yanwen Lin <lyw1124278064@gmail.com>
2026-05-20 22:05:22 -0700

    Update SwiGLU module to round to the nearest multiple of 64 for d_ff;

--- cs336_basics/model/swiglu_ff.py
+++ cs336_basics/model/swiglu_ff.py
@@ -8,7 +8,7 @@ class SwiGLU(nn.Module):
     def __init__(self, d_model: int, d_ff: int | None, device=None, dtype=None):
         super().__init__()
         assert d_model >= 64, f"d_model: {d_model} too small"
-        d_ff_rounded = d_ff if d_ff else (8 * d_model) // (3 * 64) * 64
+        d_ff_rounded = d_ff if d_ff else round(8 * d_model / (3 * 64)) * 64
         self.w1 = Linear(d_model, d_ff_rounded, dtype=dtype, device=device)
         self.w2 = Linear(d_ff_rounded, d_model, dtype=dtype, device=device)
         self.w3 = Linear(d_model, d_ff_rounded, dtype=dtype, device=device)
