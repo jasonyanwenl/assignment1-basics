7cc29df78722786c1e82ff2b629da5069a40c8a4
Yanwen Lin <lyw1124278064@gmail.com>
2026-07-05 10:32:44 -0700

    Add learning_rate_schedule function;

--- cs336_basics/functions.py
+++ cs336_basics/functions.py
@@ -31,3 +31,19 @@ def cross_entropy(
     exp = torch.exp(inputs - m)
     logsum = torch.log(torch.sum(exp, dim=-1, keepdim=True))
     return torch.mean(logsum - inputs.gather(-1, targets.unsqueeze(-1)) + m)
+
+def learning_rate_schedule(
+    it: int,
+    max_learning_rate: float,
+    min_learning_rate: float,
+    warmup_iters: int,
+    cosine_cycle_iters: int
+) -> Float:
+    if it < warmup_iters:
+        return it / warmup_iters * max_learning_rate
+    elif it <= cosine_cycle_iters:
+        return (min_learning_rate
+            + 0.5 * (1 + math.cos((it - warmup_iters) / (cosine_cycle_iters - warmup_iters) * math.pi))
+                * (max_learning_rate - min_learning_rate))
+    else:
+        return min_learning_rate
