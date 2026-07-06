bbb98a05ca943745eb9e0d04af12540cc9d66aad
Yanwen Lin <lyw1124278064@gmail.com>
2026-07-06 02:48:55 -0700

    Move decoding to a single py file;

--- cs336_basics/functions.py
+++ cs336_basics/functions.py
@@ -8,8 +8,6 @@ import numpy.typing as npt
 import torch
 from torch import Tensor
 
-from cs336_basics.model.transformer_lm import TransformerLM
-
 
 def softmax(x: Float[Tensor, "..."], dim: int) -> Float[Tensor, " ..."]:
     exp = torch.exp(x - torch.max(x, dim=dim, keepdim=True)[0])
@@ -95,38 +93,3 @@ def load_checkpoint(
     model.load_state_dict(obj["model"])
     optimizer.load_state_dict(obj["optimizer"])
     return obj["iteration"]
-
-def decoding(
-    batch_inputs: npt.NDArray,
-    model: TransformerLM,
-    context_length: int,
-    eot_token_id: int,
-    max_tokens: int=100,
-    temperature: float=1.0,
-    top_p_thres: float=None
-) -> npt.NDArray:
-    device = next(model.parameters()).device
-    with torch.no_grad():
-        batch_outputs: Int[Tensor, "batch seq"] = torch.tensor(batch_inputs, dtype=torch.long).to(device)
-        finished = torch.zeros(batch_inputs.shape[0], dtype=torch.bool, device=device)
-        model.eval()
-        for _ in range(min(max_tokens, max(0, context_length - batch_inputs.shape[1]))):
-            if finished.all():
-                break
-            out_logits: Float[Tensor, "batch vocab"] = model(batch_outputs)[:, -1]
-            prob = softmax(out_logits / temperature, dim=-1)
-
-            if top_p_thres is not None:
-                sorted_prob, token_ids_sorted_by_prob = prob.sort(dim=-1, descending=True)
-                mask = torch.zeros_like(prob, dtype=torch.bool)
-                mask[:, 1:] = sorted_prob.cumsum(dim=-1)[:, :-1] < top_p_thres
-                mask[:, 0] = True
-                raw_top_p = torch.where(mask, sorted_prob, 0.0)
-                next_token_indices = torch.multinomial(raw_top_p / raw_top_p.sum(dim=-1, keepdim=True), num_samples=1)
-                next_tokens: Int[Tensor, "batch 1"] = torch.gather(token_ids_sorted_by_prob, dim=-1, index=next_token_indices)
-            else:
-                next_tokens: Int[Tensor, "batch 1"] = torch.multinomial(prob, num_samples=1)
-
-            finished |= (next_tokens == eot_token_id).squeeze(-1)
-            batch_outputs = torch.cat((batch_outputs, next_tokens), dim=-1)
-    return batch_outputs.numpy()
